b5a34647cddea427b6f3dfb69da74168b05ffb56
henrique151 <henriqueporto949@gmail.com>
2025-10-24 18:49:15 -0300

    v3

--- app/services/teste_pipe.py
+++ app/services/teste_pipe.py
@@ -1,11 +1,8 @@
 from pipefy_service import atualizar_card_com_reuniao
 
-# ID do card que você acabou de criar
-card_id = "1243575541"
-
-# Link e data da reunião que você quer atualizar
-link_reuniao = "https://meet.google.com/exemplo"
-data_reuniao = "2025-11-01T15:00:00-03:00"  # Formato ISO 8601
+card_id = "1243592775"  # ID do card do lead
+link_reuniao = "https://meet.google.com/exemplo"  # caso tenha link
+data_reuniao = "2025-10-25T14:00:00-03:00"  # horário escolhido
 
 resultado = atualizar_card_com_reuniao(card_id, link_reuniao, data_reuniao)
 print(resultado)
